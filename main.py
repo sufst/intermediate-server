b729c9f0d630cfee691000aa945423d701c87ee4
Nathanrs97 <nathan-rowley@hotmail.co.uk>
2020-12-26 17:19:19 +0000

    Added initial Pycharm project and command line arguments --port --ip --verbose --baud --com. Closes #2.

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+    Southampton University Formula Student Team Intermediate Server
+    Copyright (C) 2020 Nathan Rowley-Smith
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create an intermediate server.")
+
+    parser.add_argument("--port", type=int, default=19900, help="The port to host the server on.")
+    parser.add_argument("--ip", type=str, default="127.0.0.1", help="The IP address to host the server on.")
+    parser.add_argument("--verbose", type=str, default="INFO",
+                        help="The verbose level of the server: DEBUG, INFO, WARN, ERROR")
+    parser.add_argument("--baud", type=int, default=115200, help="Baud rate for the attached XBee.")
+    parser.add_argument("--com", type=str, default="COM0", help="Com port for the attached XBee e.g. COM1")
+
+    print("Intermediate Server  Copyright (C) 2020  Nathan Rowley-Smith\n" +
+          "This program comes with ABSOLUTELY NO WARRANTY;\n" +
+          "This is free software, and you are welcome to redistribute it")
+
+    args = parser.parse_args()
+    print(args)
