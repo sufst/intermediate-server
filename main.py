ed4cc3f4fde3f07d5c0c3ecccfb45d2d1942f7ff
Nathanrs97 <nathan-rowley@hotmail.co.uk>
2020-12-26 21:29:34 +0000

    This commit implements the asyncio transport and protocols create_server for creating a TCP server for serving TCP connections. UDP is not currently support as I do not see why UDP would be used over TCP. However, it may be found during testing that UDP is required because of the its overheads. Issue #4 is mostly covered by this commit. However, some XBee functionality has also been implemenmted in this commit to standardise the receiving handling accross XBee and sockets #3.

--- main.py
+++ main.py
@@ -17,6 +17,9 @@
 """
 from __future__ import annotations
 import argparse
+from typing import Optional
+from typing import Callable
+
 import common
 from digi.xbee import devices as xbee_devices
 from digi.xbee.models import message as xbee_message
@@ -24,7 +27,64 @@ import asyncio
 
 
 class Server:
-    def __init__(self, ip: str, port: int, com: str, baud: int, verbose: str) -> None:
+    class _ServerProtocolFactory(asyncio.Protocol):
+        def __init__(self, async_recv_cb: Callable[[bytes], asyncio.coroutine], verbose: str) -> None:
+            """
+            Initialise the asyncio Protocol class.
+            See https://docs.python.org/3/library/asyncio-protocol.html for details of asyncio protocol and transports.
+            :param verbose:
+            """
+            super().__init__()
+            self._transport = None
+            self._logger = None
+            self._peer_ip = ""
+            self._peer_port = ""
+            self._verbose = verbose
+            self._async_recv_cb = async_recv_cb
+
+        def connection_made(self, transport: asyncio.transports.BaseTransport) -> None:
+            """
+            Invoked when a new connected is made from a client.
+            :param transport:   The transport object corresponding to this new connection.
+            """
+            self._peer_ip, self._peer_port = transport.get_extra_info("peername")
+            self._logger = common.get_logger(f"{self._peer_ip}-{self._peer_port}", self._verbose)
+            self._logger.info(f"New connection")
+            self._transport = transport
+
+        def _on_async_recv_cb_done(self, future: asyncio.Future) -> None:
+            """
+            Invoked once the _on_async_recv_cb_done coroutine has finished.
+            This means when _handle_recv has finished handling the frame and returns a byte array (or None) to send back
+            to the peer.
+            :param future:  The future that is done.
+            """
+            out = future.result()
+            self._logger.debug(f"Recv callback finished with outcome {out}")
+            if out is not None:
+                self._logger.info(f"<- {out}")
+                self._transport.write(out)
+
+        def data_received(self, data: bytes) -> None:
+            """
+            Invoked when data is received from the connected peer.
+            :param data:    The received data.
+            """
+            self._logger.info(f"-> {data}")
+            asyncio.create_task(self._async_recv_cb(data)).add_done_callback(self._on_async_recv_cb_done)
+
+        def connection_lost(self, exc: Optional[Exception]) -> None:
+            """
+            Invoked when the peer is lost.
+            :param exc: An Exception if any occurred.
+            """
+            self._logger.warning("Lost connection")
+            if exc is not None:
+                self._logger.error(f"{repr(exc)}")
+
+            del self
+
+    def __init__(self, ip: str, port: int, com: str, baud: int, xbee_mac: str, verbose: str) -> None:
         """
         Initialise the Server singleton instance.
         :param ip:      IP to host the server on.
@@ -41,9 +101,10 @@ class Server:
         self._verbose = verbose
 
         self._event_loop = None
-        self._event_stop = None
 
         self._xbee = xbee_devices.XBeeDevice(com, baud)
+        self._xbee_remote = xbee_devices.RemoteXBeeDevice(self._xbee, xbee_devices.XBee64BitAddress.from_hex_string(
+            xbee_mac))
 
     def __enter__(self) -> Server:
         """
@@ -54,22 +115,39 @@ class Server:
         self._xbee.add_data_received_callback(self._xbee_data_recv_cb)
         return self
 
+    def _on_async_xbee_data_handle_done(self, future: asyncio.Future) -> None:
+        """
+        Invoked once the _handle_recv has finished as a result of an xbee received message.
+        Used to handle sending any response back to the XBee.
+        :param future:  The done future.
+        """
+        out = future.result()
+        self._logger.info(f"XBee message handle done with outcome {out}")
+        if out is not None:
+            self._logger.info(f"Xbee <- {out}")
+            self._xbee.send_data_async(self._xbee_remote, out)
+
     def _xbee_data_recv_cb(self, message: xbee_message.XBeeMessage) -> None:
         """
         XBee call back function for when xbee data is received.
         NOTE: This is called from an XBee library thread and not the server thread.
         :param message: The received message.
         """
-        self._logger.info(f"XBee <- {message.data}")
-        asyncio.run_coroutine_threadsafe(self._handle_recv(message.data), self._event_loop)
+        data = bytes(message.data)
+        self._logger.info(f"XBee -> {data}")
 
-    async def _handle_recv(self, frame: bytes) -> None:
+        asyncio.run_coroutine_threadsafe(self._handle_recv(data), self._event_loop).add_done_callback(
+            self._on_async_xbee_data_handle_done)
+
+    async def _handle_recv(self, frame: bytes) -> Optional[bytes]:
         """
         Handle a received frame asynchronously. This could be from either the XBee receive callback or from the
         server protocol factory.
         :param frame: The received frame.
         """
         self._logger.info(f"Handling raw frame: {frame}")
+
+        return frame
 
     def run(self) -> None:
         """
@@ -81,14 +159,21 @@ class Server:
         """
         The asyncio event loop for the server.
         """
-        self._event_stop = asyncio.Event()
-
         self._logger.info("Running")
         self._event_loop = asyncio.get_running_loop()
-        await self._event_stop.wait()
+
+        self._logger.info(f"Creating server {self._ip}:{self._port}")
+        server_factory = await self._event_loop.create_server(
+            lambda: self._ServerProtocolFactory(self._handle_recv, self._verbose), self._ip, self._port)
+
+        self._logger.info(f"Server created")
+        async with server_factory:
+            await server_factory.serve_forever()
+
         self._logger.info("Stopped")
 
-    def __exit__(self, exc_type: object, exc_val: object, exc_tb: object) -> None:
+    def __exit__(self, exc_type: Optional[Exception], exc_val: Optional[Exception], exc_tb: Optional[Exception]) \
+            -> None:
         """
         Exit for use with "with as"
         :param exc_type:
@@ -111,6 +196,7 @@ if __name__ == '__main__':
                         help="The verbose level of the server: DEBUG, INFO, WARN, ERROR")
     parser.add_argument("--baud", type=int, default=115200, help="Baud rate for the attached XBee.")
     parser.add_argument("--com", type=str, default="COM0", help="Com port for the attached XBee e.g. COM1")
+    parser.add_argument("--mac", type=str, default="FFFFFFFFFFFFFFFF", help="MAC address of car XBee.")
 
     print("Intermediate Server  Copyright (C) 2020  Nathan Rowley-Smith\n" +
           "This program comes with ABSOLUTELY NO WARRANTY;\n" +
@@ -121,5 +207,5 @@ if __name__ == '__main__':
     logger = common.get_logger("root", "DEBUG")
     logger.info(args.__dict__)
 
-    with Server(args.ip, args.port, args.com, args.baud, args.verbose) as server:
+    with Server(args.ip, args.port, args.com, args.baud, args.mac, args.verbose) as server:
         server.run()
