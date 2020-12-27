6ab216708da6aebbb35b3e570be4ca159174e795
Nathanrs97 <nathan-rowley@hotmail.co.uk>
2020-12-27 02:43:52 +0000

    Added the rest of the frames. All frames are sent in turn with dummy increasing values every 1 second. Mostly resolves #8 but still need to implement XBee support.

--- car_emulator.py
+++ car_emulator.py
@@ -39,8 +39,14 @@ class Client:
             self._logger = logger
             self._transport = None
             self._seq_num = 1
-            self._last_sent_pdu = None
+
+            self._aipdu = protocol.ProtocolAIPDU()
             self._acpdu = protocol.ProtocolACPDU()
+            self._aapdu = protocol.ProtocolAAPDU()
+            self._adpdu = protocol.ProtocolADPDU()
+            self._appdu = protocol.ProtocolAPPDU()
+            self._aspdu = protocol.ProtocolASPDU()
+            self._ampdu = protocol.ProtocolAMPDU()
 
         def connection_made(self, transport: asyncio.Transport) -> None:
             """
@@ -50,21 +56,182 @@ class Client:
             self._transport = transport
             self._logger.info("Connection made")
 
-            aipdu = protocol.ProtocolAIPDU()
-            aipdu.header.seq_num = self._seq_num
-            self._seq_num += 1
-            now = datetime.datetime.now()
-            aipdu.header.epoch = int((now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            out = self._get_aipdu_raw()
 
-            aipdu.client_type = protocol.CAR_EMULATOR
-            aipdu.sw_ver = _SW_VERSION
-            aipdu.client_name = "emulator"
-
-            out = aipdu.pack_raw()
-            self._logger.info(f"{str(aipdu)}")
             self._logger.info(f"<- {out}")
             self._transport.write(out)
-            self._last_sent_pdu = aipdu
+
+        async def _periodic_pdu_transmit(self):
+            await asyncio.sleep(1)
+            out = self._get_acpdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            await asyncio.sleep(1)
+            out = self._get_aapdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            await asyncio.sleep(1)
+            out = self._get_adpdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            await asyncio.sleep(1)
+            out = self._get_appdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            await asyncio.sleep(1)
+            out = self._get_aspdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            await asyncio.sleep(1)
+            out = self._get_ampdu_raw()
+            self._logger.info(f"<- {out}")
+            self._transport.write(out)
+
+            asyncio.create_task(self._periodic_pdu_transmit())
+
+        def _get_aipdu_raw(self) -> bytes:
+            """
+            Get an AIPDU frame in raw format.
+            """
+            self._aipdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._aipdu.header.epoch = int((now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+
+            self._aipdu.client_type = protocol.CAR_EMULATOR
+            self._aipdu.sw_ver = _SW_VERSION
+            self._aipdu.client_name = "emulator"
+
+            out = self._aipdu.pack_raw()
+            self._logger.info(f"{str(self._aipdu)}")
+
+            return out
+
+        def _get_acpdu_raw(self) -> bytes:
+            """
+            Get an ACPDU frame in raw format.
+            """
+            self._acpdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._acpdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._acpdu.rpm += 1
+            self._acpdu.water_temp += 1
+            self._acpdu.tps_perc += 1
+            self._acpdu.battery_mv += 1
+            self._acpdu.external_5v_mv += 1
+            self._acpdu.fuel_flow += 1
+            self._acpdu.lambda_val += 1
+            self._acpdu.speed_kph += 1
+
+            out = self._acpdu.pack_raw()
+            self._logger.info(f"{str(self._acpdu)}")
+
+            return out
+
+        def _get_aapdu_raw(self) -> bytes:
+            """
+            Get an AAPDU frame in raw format.
+            """
+            self._aapdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._aapdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._aapdu.evo_scanner1 += 1
+            self._aapdu.evo_scanner2 += 1
+            self._aapdu.evo_scanner3 += 1
+            self._aapdu.evo_scanner4 += 1
+            self._aapdu.evo_scanner5 += 1
+            self._aapdu.evo_scanner6 += 1
+            self._aapdu.evo_scanner7 += 1
+
+            out = self._aapdu.pack_raw()
+            self._logger.info(f"{str(self._aapdu)}")
+
+            return out
+
+        def _get_adpdu_raw(self) -> bytes:
+            """
+            Get an ADPDU frame in raw format.
+            """
+            self._adpdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._adpdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._adpdu.ecu_status = protocol.ECU_STATUS_CONNECTED
+            self._adpdu.engine_status = protocol.ENGINE_STATUS_IDLE
+            self._adpdu.battery_status = protocol.BATTERY_STATUS_HEALTHY
+            self._adpdu.car_logging_status = protocol.CAR_LOGGING_STATUS_OFF
+
+            out = self._adpdu.pack_raw()
+            self._logger.info(f"{str(self._adpdu)}")
+
+            return out
+
+        def _get_appdu_raw(self) -> bytes:
+            """
+            Get an APPDU frame in raw format.
+            """
+            self._appdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._appdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._appdu.injection_time += 1
+            self._appdu.injection_duty_cycle += 1
+            self._appdu.lambda_pid_adjust += 1
+            self._appdu.lambda_pid_target += 1
+
+            out = self._appdu.pack_raw()
+            self._logger.info(f"{str(self._appdu)}")
+
+            return out
+
+        def _get_aspdu_raw(self) -> bytes:
+            """
+            Get an ASPDU frame in raw format.
+            """
+            self._aspdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._aspdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._aspdu.ride_height_fl_cm += 1
+            self._aspdu.ride_height_fr_cm += 1
+            self._aspdu.ride_height_flw_cm += 1
+            self._aspdu.ride_height_rear_cm += 1
+
+            out = self._aspdu.pack_raw()
+            self._logger.info(f"{str(self._aspdu)}")
+
+            return out
+
+        def _get_ampdu_raw(self) -> bytes:
+            """
+            Get an AMPDU frame in raw format.
+            """
+            self._ampdu.header.seq_num = self._seq_num
+            self._seq_num += 1
+            now = datetime.datetime.now()
+            self._ampdu.header.epoch = int(
+                (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+            self._ampdu.lap_timer_s += 1
+            self._ampdu.accel_fl_x_mg += 1
+            self._ampdu.accel_fl_y_mg += 1
+            self._ampdu.accel_fl_z_mg += 1
+
+            out = self._ampdu.pack_raw()
+            self._logger.info(f"{str(self._ampdu)}")
+
+            return out
 
         def data_received(self, data: bytes) -> None:
             """
@@ -77,28 +244,13 @@ class Client:
             if pdu is not None:
                 self._logger.info(str(pdu))
                 if pdu.header.frame_type == protocol.AIPDU:
-                    if pdu == self._last_sent_pdu:
+                    if pdu == self._aipdu:
                         self._logger.info("AIPDU match")
+                        out = self._get_acpdu_raw()
 
-                        self._acpdu.header.seq_num = self._seq_num
-                        self._seq_num += 1
-                        now = datetime.datetime.now()
-                        self._acpdu.header.epoch = int(
-                            (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
-                        self._acpdu.rpm += 1
-                        self._acpdu.water_temp += 1
-                        self._acpdu.tps_perc += 1
-                        self._acpdu.battery_mv += 1
-                        self._acpdu.external_5v_mv += 1
-                        self._acpdu.fuel_flow += 1
-                        self._acpdu.lambda_val += 1
-                        self._acpdu.speed_kph += 1
-
-                        out = self._acpdu.pack_raw()
-                        self._logger.info(f"{str(self._acpdu)}")
                         self._logger.info(f"<- {out}")
                         self._transport.write(out)
-                        self._last_sent_pdu = self._acpdu
+                        asyncio.create_task(self._periodic_pdu_transmit())
                     else:
                         self._logger.error("AIPDU do not match")
 
