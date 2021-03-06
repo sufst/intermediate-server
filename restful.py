d20727fe138b1b5f295db1c27a2f3677aee36d95
Nathanrs97 <nathan-rowley@hotmail.co.uk>
2021-03-06 12:15:55 +0000

    An initial commit of the RESTful functionality implemented in restful.py within Restful. Basic units tests have been added along side the initial implementation in test_restful.py.
    The RESTful implementation is built upon the websocket library https://websockets.readthedocs.io/en/stable/intro.html. The API of restful.py is similar to the websocket library where a serve function is used to register a callable function which is invoked when RESTful requests are received to be handled. The RestfulRequest is a subclass which encapsulates the request for the callable function along with a respond function which the callable function will use to respond to the request.

---
+++ restful.py
@@ -0,0 +1,93 @@
+"""
+    Southampton University Formula Student Team Intermediate Server
+    Copyright (C) 2020 Nathan Rowley-Smith
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import asyncio
+import json
+
+import websockets
+
+import common
+
+
+class RestfulRequest:
+    def __init__(self, websocket, request_str: str):
+        self._request_str = request_str
+        self._websocket = websocket
+
+        self._dataset, self._filters, self._type = None, [], None
+
+        self._logger = common.get_logger("Restful", "DEBUG")
+
+        self._decode_request()
+
+    def _decode_request(self):
+        # Decode GET /sensors/RPM?timesince=<epoch>&amount=<n>
+        try:
+            split_space = self._request_str.split(" ")
+            self._type = split_space[0]
+            split_question = split_space[1].split("?")
+            self._dataset = split_question[0]
+            split_and = split_question[1].split("&")
+
+            for fil in split_and:
+                self._filters.extend([tuple(fil.split("="))])
+        except Exception as error:
+            self._logger.error(repr(error))
+            self._websocket.send({"ERROR": [{"type": repr(error)}]})
+            self._websocket.close()
+
+    async def respond(self, response: dict):
+        await self._websocket.send(json.dumps(response))
+        await self._websocket.close()
+
+    def get_type(self):
+        return self._type
+
+    def get_dataset(self):
+        return self._dataset
+
+    def get_filters(self):
+        return self._filters
+
+    def __str__(self):
+        return str({"type": self._type, "dataset": self._dataset, "filters": self._filters})
+
+
+class Restful:
+    def __init__(self, url: str, port: int):
+        self._url, self._port = url, port
+        self._server = websockets.serve(self._websocket_serve, self._url, self._port)
+        self._request_callable = None
+
+        self._logger = common.get_logger("Restful", "DEBUG")
+
+    def serve(self, request_callable):
+        self._request_callable = request_callable
+
+        asyncio.get_event_loop().run_until_complete(self._server)
+        self._logger.info(f"Serving on {self._url}:{self._port}")
+
+    async def _websocket_serve(self, websocket, path: str):
+        try:
+            request = await websocket.recv()
+            self._logger.info(f"Got request: {request}")
+
+            request = RestfulRequest(websocket, request)
+
+            await self._request_callable(request)
+        except websockets.ConnectionClosedOK:
+            pass
---
+++ test_restful.py
@@ -0,0 +1,50 @@
+import asyncio
+import json
+import unittest
+
+import websockets
+
+import restful
+
+
+async def _restful_serve(request: restful.RestfulRequest):
+    print(request)
+
+    if request.get_type() == "GET":
+        if request.get_dataset() == "/sensors":
+            for req_filter in request.get_filters():
+                filter_name, filter_value = req_filter
+                await request.respond({filter_name: int(filter_value)})
+
+
+async def _test_connect():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        await websocket.close()
+
+
+async def _test_request():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        await websocket.send("GET /sensors?amount=10")
+
+        response_json = await websocket.recv()
+        response = json.loads(response_json)
+
+        print(response)
+
+        if not response == {"amount": 10}:
+            raise Exception
+
+        await websocket.close()
+
+
+class TestRestfulConnect(unittest.TestCase):
+    def test_connect(self):
+        restful.Restful("localhost", 8765).serve(_restful_serve)
+        asyncio.get_event_loop().run_until_complete(_test_connect())
+
+    def test_request(self):
+        asyncio.get_event_loop().run_until_complete(_test_request())
+
+
+if __name__ == '__main__':
+    unittest.main()
