b9e271b071ba4a597fb901cb3359c75a638cc580
Nathanrs97 <nathan-rowley@hotmail.co.uk>
2021-05-04 18:57:31 +0100

    Update

--- src/plugins/schema.py
+++ src/plugins/schema.py
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from helpers import config
+from src.helpers import config
 import functools
 import asyncio
 import serial
